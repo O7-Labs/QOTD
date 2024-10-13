e14c5c9b9e10f1d70401dcdb423514d21a17a760
RiyaParikh0112 <moodycodes@gmail.com>
2024-10-13 21:23:19 +0530

    v1

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import QuizGame from '../../components/QuizGame'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <QuizGame />
+    </main>
+  )
+}
