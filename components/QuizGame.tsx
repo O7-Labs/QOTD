919a5f5943c381fc5cee9391be4f256bf9aac507
RiyaParikh0112 <moodycodes@gmail.com>
2024-10-16 22:19:33 +0530

    css fix

--- components/QuizGame.tsx
+++ components/QuizGame.tsx
@@ -175,152 +175,137 @@ export default function QuizGame() {
     setShowToast(true);
     setTimeout(() => setShowToast(false), 3000); // Toast disappears after 3 seconds
   };
-//   const fetchQuestionOfTheDay = async () => {
-//     const today = new Date().toISOString().slice(0, 10); // Format as "YYYY-MM-DD"
+
+
   
-//     let { data: questionData, error } = await supabase
-//       .from('questions')
-//       .select('*')
-//       .eq('date', today)
-//       .single();
+    if (!question) return <div>Loading...</div>
   
-//     if (error) {
-//       console.error('Error fetching question:', error);
-//       return null;
-//     }
-  
-//     return questionData;
-//   };
-
-
-
-  if (!question) return <div>Loading...</div>
-
-  return (
-
-    <div className="quix-container">
-      <header>
-        <h1 className="quix-title">QUIX</h1>
-      </header>
-      <div className="question-header">
-        Question of the day
-      </div>
-      <div className="question-content">
-        <p className={`${isQuestionExpanded ? "" : "line-clamp-2"}`}>
-          {question.question}
-        </p>
-        {question.question.length > 100 && (
-          <button
-            className="expand-button"
-            onClick={() => setIsQuestionExpanded(!isQuestionExpanded)}
-          >
-            <ChevronDown
-              className={`w-4 h-4 transition-transform ${
-                isQuestionExpanded ? "rotate-180" : ""
-              }`}
-            />
-          </button>
-        )}
-        {question.image_url && question.image_url !== "NULL" && (
-          <div className="question-image">
-            <Image
-              src={question.image_url}
-              alt="Question image"
-              width={500}  // Set the original width of the image
-              height={300} // Set the original height of the image
-              layout="responsive"
-            />
+    return (
+      <>
+        <header className="quix-header">
+          <h1 className="quix-title">QUIX</h1>
+        </header>
+        <div className="quix-container">
+          <div className="question-header">
+            Question of the day
           </div>
-        )}
-      </div>
-      <div className="attempts-container">
-        {attempts.map((attempt, index) => (
-          <div key={index} className="attempt-row">
-            {attempt.split("").map((letter, letterIndex) => (
-              <div
-                key={letterIndex}
-                className={`attempt-block ${getFeedbackColor(feedback[index][letterIndex])}`}
+          <div className="question-content">
+            <p className={`${isQuestionExpanded ? "" : "line-clamp-2"}`}>
+              {question.question}
+            </p>
+            {question.question.length > 100 && (
+              <button
+                className="expand-button"
+                onClick={() => setIsQuestionExpanded(!isQuestionExpanded)}
               >
-                {letter}
+                <ChevronDown
+                  className={`w-4 h-4 transition-transform ${
+                    isQuestionExpanded ? "rotate-180" : ""
+                  }`}
+                />
+              </button>
+            )}
+            {question.image_url && question.image_url !== "NULL" && (
+              <div className="question-image">
+                <Image
+                  src={question.image_url}
+                  alt="Question image"
+                  width={500}  // Set the original width of the image
+                  height={300} // Set the original height of the image
+                  layout="responsive"
+                />
+              </div>
+            )}
+          </div>
+          <div className="attempts-container">
+            {attempts.map((attempt, index) => (
+              <div key={index} className="attempt-row">
+                {attempt.split("").map((letter, letterIndex) => (
+                  <div
+                    key={letterIndex}
+                    className={`attempt-block ${getFeedbackColor(feedback[index][letterIndex])}`}
+                  >
+                    {letter}
+                  </div>
+                ))}
               </div>
             ))}
           </div>
-        ))}
-      </div>
-      <div className="answer-container">
-        {question.answer.split("").map((char, index) => (
-          char === " " ? (
-            <div key={index} className="answer-space" />
-          ) : (
-            <input
-              key={index}
-              className="answer-input"
-              maxLength={1}
-              value={answer[index] || ""}
-              onChange={(event) => handleCharacterChange(index, event.target.value)}
-              disabled={gameState !== "playing"}
-            />
-          )
-        ))}
-      </div>
-      <button
-        className="submit-button"
-        onClick={() => handleSubmit(answer)}
-        disabled={gameState !== "playing"}
-      >
-        Submit
-      </button>
-
-      {showExplanation && (
-        <div className="explanation">
-          <h3>Answer Explanation:</h3>
-          <p>{question?.explanation}</p>
-          {question?.image_url && question.image_url !== "NULL" && (
-            <div className="explanation-image">
-              <Image
-                src={question.image_url}
-                alt="Explanation image"
-                width={500}  // Set the original width of the image
-                height={300} // Set the original height of the image
-                layout="responsive"
-              />
+          <div className="answer-container">
+            {question.answer.split("").map((char, index) => (
+              char === " " ? (
+                <div key={index} className="answer-space" />
+              ) : (
+                <input
+                  key={index}
+                  className="answer-input"
+                  maxLength={1}
+                  value={answer[index] || ""}
+                  onChange={(event) => handleCharacterChange(index, event.target.value)}
+                  disabled={gameState !== "playing"}
+                />
+              )
+            ))}
+          </div>
+          <button
+            className="submit-button"
+            onClick={() => handleSubmit(answer)}
+            disabled={gameState !== "playing"}
+          >
+            Submit
+          </button>
+  
+          {showExplanation && (
+            <div className="explanation">
+              <h3>Answer Explanation:</h3>
+              <p>{question?.explanation}</p>
+              {question?.image_url && question.image_url !== "NULL" && (
+                <div className="explanation-image">
+                  <Image
+                    src={question.image_url}
+                    alt="Explanation image"
+                    width={500}  // Set the original width of the image
+                    height={300} // Set the original height of the image
+                    layout="responsive"
+                  />
+                </div>
+              )}
             </div>
           )}
-        </div>
-      )}
-      {gameState === "finished" && (
-          <div className="statistics-panel">
-            <h3>Statistics</h3>
-            <div className="stats-grid">
-              <div className="stat">
-                <div className="stat-value">{stats.played}</div>
-                <div className="stat-label">Played</div>
+          {gameState === "finished" && (
+              <div className="statistics-panel">
+                <h3>Statistics</h3>
+                <div className="stats-grid">
+                  <div className="stat">
+                    <div className="stat-value">{stats.played}</div>
+                    <div className="stat-label">Played</div>
+                  </div>
+                  <div className="stat">
+                    <div className="stat-value">{stats.winPercentage}%</div>
+                    <div className="stat-label">Win %</div>
+                  </div>
+                  <div className="stat">
+                    <div className="stat-value">{stats.currentStreak}</div>
+                    <div className="stat-label">Current Streak</div>
+                  </div>
+                  <div className="stat">
+                    <div className="stat-value">{stats.maxStreak}</div>
+                    <div className="stat-label">Max Streak</div>
+                  </div>
+                </div>
+                <button className="copy-button" onClick={handleCopy}>Copy</button>
+                <button className="explore-button">Explore previous QOTD</button>
               </div>
-              <div className="stat">
-                <div className="stat-value">{stats.winPercentage}%</div>
-                <div className="stat-label">Win %</div>
-              </div>
-              <div className="stat">
-                <div className="stat-value">{stats.currentStreak}</div>
-                <div className="stat-label">Current Streak</div>
-              </div>
-              <div className="stat">
-                <div className="stat-value">{stats.maxStreak}</div>
-                <div className="stat-label">Max Streak</div>
-              </div>
+            )}
+        {showToast && <div className="toast">Copied to clipboard</div>}
+          <div className="footer">
+            <div className="timer">
+              <Clock className="w-4 h-4" />
+              <span>{formatTime(timeElapsed)}</span>
             </div>
-            <button className="copy-button" onClick={handleCopy}>Copy</button>
-            <button className="explore-button">Explore previous QOTD</button>
+            <span className="credits">Made with ❤️ o7 labs</span>
           </div>
-        )}
-    {showToast && <div className="toast">Copied to clipboard</div>}
-      <div className="footer">
-        <div className="timer">
-          <Clock className="w-4 h-4" />
-          <span>{formatTime(timeElapsed)}</span>
         </div>
-        <span className="credits">Made with ❤️ o7 labs</span>
-      </div>
-    </div>
-  )
-}
+      </>
+    )
+  }
